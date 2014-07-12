87a979080874b7db1f82524cbf3a2b34c835eede
jgerardsimcock <justin_simcock@yahoo.com>
2014-07-12 10:07:56 -1000

    Refactor

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,15 +1,15 @@
 function pixelPainter(width, height) {
 
 
-  var artboard = $('#artboard'); //creates variable for #artboard
-  var grid = $('<table id="grid"></table>'); //creates variable for table, that creates a table and that will appended to #artboard 
+  this.artboard = $('<div id=pixelPainter_artboard></div>'); //creates variable for #artboard
+  var grid = $('<table id="grid"></table>');    //creates variable for table, that creates a table and that will appended to #artboard 
 
-  var controls = $('#controls');        //creates variable for #controls
+  this.controls = $('<div id=pixelPainter_controls></div>');        //creates variable for #controls
   var palette = $('<table id="palette"></table>'); //creates variable and a table for pallette that will be appended to #controls
   
   //COLOR ARRAY
   var colors = ['#000000', '#003300', '#006600', '#009900', '#00FF00', '#000066', '#003366', '#00FF66', '#333366', '#660066', '#663366', '#660000', '#660033', '#990000', '#800000', '#99FF00', '#CC0000', '#990099', '#996699', '#CC0066', '#CC9966', '#FF0066', '#FFFF66', '#0000CC', '#FF00CC'];
-  var clickedColor;//EMPTY VAR THAT HOLDS THE COLOR OF LAST COLOR CLICKED
+  var clickedColor;             //EMPTY VAR THAT HOLDS THE COLOR OF LAST COLOR CLICKED
     console.log(colors.length);
 
   var erase = $('<button id="erase" value="Erase">Erase</button>'); //initializes Erase button
@@ -19,7 +19,7 @@ function pixelPainter(width, height) {
   $('body').append(clear);
  
 
-//THIS IS THE BLANK GRID
+  //THIS IS THE BLANK GRID
 
   for (var i = 0; i < width; i++){                //this creates table rows through a for loop
     var row = $('<tr></tr>');                     //this initializes a variable for each table row so that it can later be appended to table
@@ -30,7 +30,7 @@ function pixelPainter(width, height) {
       };
   };
 
-  artboard.append(grid);  //appends table we just created to the artboard div in DOM
+  artboard.append(grid);                          //appends table we just created to the artboard div in DOM
 
   
 
@@ -45,8 +45,8 @@ function pixelPainter(width, height) {
     for (var l = 0; l < 5; l++) {
       var column = $('<td class="palette-cell"></td>');
         row.append(column);
-        column.css({'background-color': colors[m]}); //THIS SETS BACKGROUND COLOR TO THE VALUE AT COLORS[M]
-        m++; //THIS TELLS THE PROGRAM TO CHANGE THE VALUE OF M WHICH HLEPS MOVE THROUGH THE COLOR ARRAY
+        column.css({'background-color': colors[m]});   //THIS SETS BACKGROUND COLOR TO THE VALUE AT COLORS[M]
+        m++;    //THIS TELLS THE PROGRAM TO CHANGE THE VALUE OF M WHICH HLEPS MOVE THROUGH THE COLOR ARRAY
     };
   };
 
@@ -56,12 +56,13 @@ function pixelPainter(width, height) {
 
   //THIS SECTION TRANSFERS COLOR FROM THE PALETTE TO THE GRID
 
-  $('.palette-cell').click(function() {                  //initiates the event handler with a click on the color pallette
-    clickedColor = $(this).css('background-color');//this holds the color of the clicked tile in variable clickedColor
+  $("#controls").on('click', '.palette-cell', function() {     //initiates the event handler with a click on the color pallette
+    console.log("this");
+    clickedColor = $(this).css('background-color');           //this holds the color of the clicked tile in variable clickedColor
     });
 
-  $('.grid-cell').click(function(){                //initiates event handler with a click on grid cell
-    $(this).css("background-color", clickedColor); //transfers color of most recently clickedColor to cell that was just clicked
+  $("#artboard").on('click', '.grid-cell', function(){       //initiates event handler with a click on grid cell
+    $(this).css("background-color", clickedColor);           //transfers color of most recently clickedColor to cell that was just clicked
   });
 
   // THIS REMOVES THE LAST COLOR CLICKED
@@ -78,13 +79,16 @@ function pixelPainter(width, height) {
     $('.grid-cell').css("background-color", "white");
   });
 
-  };       
+  return this;
+};       
     
 
 
-  $(document).ready(function(){
-    pixelPainter(10,10);
-  });
+$(document).ready(function(){
+  var pixelPainterVariable = pixelPainter(10,10);
+  $("#controls").append(pixelPainterVariable.controls);
+  $("#artboard").append(pixelPainterVariable.artboard);
+});
 
 
 
