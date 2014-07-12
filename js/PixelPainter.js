abc1b36bb5aad274c5bebe10631641a55f44c11d
jgerardsimcock <justin_simcock@yahoo.com>
2014-07-12 08:01:02 -1000

    Submit Commit

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,68 +1,90 @@
 function pixelPainter(width, height) {
 
 
+  var artboard = $('#artboard'); //creates variable for #artboard
+  var grid = $('<table id="grid"></table>'); //creates variable for table, that creates a table and that will appended to #artboard 
 
-  var artboard = $('#artboard');
-  var controls = $('#controls');        //this makes the div with id controls a variable that can later be invoked
-  var table1 = $('<table id="table1"></table>');
-  var table2 = $('<table id="table2"></table>');
-  var colors = [ '#000000', '#003300', '#006600', '#009900', '#00FF00', '#000066', '#003366', '#00FF66', '#333366', '#660066', '#663366', '#660000', '#660033', '＃990000'，'＃999900', '#99FF00', '#CC0000', '#990099', '#996699', '#CC0066', '#CC9966', '#FF0066', '#FFFF66', '#0000CC', '#FF00CC'];
-  var clickedColor;
-
-
-
+  var controls = $('#controls');        //creates variable for #controls
+  var palette = $('<table id="palette"></table>'); //creates variable and a table for pallette that will be appended to #controls
   
-  for (var i = 0; i < width; i++){            //this creates table rows through a for loop
-    var row = $('<tr></tr>');                  //this initializes a variable for each table row so that it can later be appended to table
-    table1.append(row);                       //appends tr's to table
+  //COLOR ARRAY
+  var colors = ['#000000', '#003300', '#006600', '#009900', '#00FF00', '#000066', '#003366', '#00FF66', '#333366', '#660066', '#663366', '#660000', '#660033', '#990000', '#800000', '#99FF00', '#CC0000', '#990099', '#996699', '#CC0066', '#CC9966', '#FF0066', '#FFFF66', '#0000CC', '#FF00CC'];
+  var clickedColor;//EMPTY VAR THAT HOLDS THE COLOR OF LAST COLOR CLICKED
+    console.log(colors.length);
 
-      for (var j = 0; j < height; j++) {      //the loop that iterates the number of times equal to the height of the grid
-       var column = $('<td class="cell"></td>'); //this initializes and creates individual columns, 
-        row.append(column);             //this appends the columns to each row
+  var erase = $('<button id="erase" value="Erase">Erase</button>'); //initializes Erase button
+  var clear = $('<button id="clear" value="Clear">Clear</button>'); //initializes Clear button
+
+  $('body').append(erase);
+  $('body').append(clear);
+ 
+
+//THIS IS THE BLANK GRID
+
+  for (var i = 0; i < width; i++){                //this creates table rows through a for loop
+    var row = $('<tr></tr>');                     //this initializes a variable for each table row so that it can later be appended to table
+      grid.append(row);                           //appends tr's to table
+      for (var j = 0; j < height; j++) {          //the loop that iterates the number of times equal to the height of the grid
+       var column = $('<td class="grid-cell"></td>');  //this initializes and creates individual columns,
+        row.append(column);                       //this appends the columns to each row
       };
   };
-  controls.append(table1);             //appends table we just created to the controls div in DOM
 
+  artboard.append(grid);  //appends table we just created to the artboard div in DOM
 
-
-
-
-for(var i = 0; i < 5; i++){
-   var row = $('<tr></tr>'); 
-   table2.append(row);
-
-    for (var j = 0; j < 5; j++) {
-      var column = $('<td class="cell2"></td>');
-        row.append(column);
-    };
-    for(var i = 0; i < colors.length; i++){
-      column.css("background-color", colors[i]);
-    };
-};
-    artboard.append(table2);
-
-
-
-
-
-  // td.click(function() {                  //initiates the event handler with a click on the color pallette
-
-  //   console.log("click1")
-  //   clickedColor = $(this).css('background-color');//this holds the color of the clicked tile in variable clickedColor
-  //   console.log(clickedColor);
-  //   });
-
-  // cell.click(function(){                //initiates event handler with a click on grid cell
-  //     console.log(clickedColor); 
-  //     $(this).css("background-color", clickedColor); //transfers color of most recently clickedColor to cell that was just clicked
-  // });
-
-
-};
   
-$(document).ready(function(){
-  pixelPainter(10,10);
-});
+
+
+  //THIS CREATES THE COLOR CONTROL PALETTE
+
+  var m = 0 //This sets the color counter at INDEX zero
+
+  for(var k = 0; k < 5; k++){ 
+   var row = $('<tr></tr>'); 
+    palette.append(row);
+    for (var l = 0; l < 5; l++) {
+      var column = $('<td class="palette-cell"></td>');
+        row.append(column);
+        column.css({'background-color': colors[m]}); //THIS SETS BACKGROUND COLOR TO THE VALUE AT COLORS[M]
+        m++; //THIS TELLS THE PROGRAM TO CHANGE THE VALUE OF M WHICH HLEPS MOVE THROUGH THE COLOR ARRAY
+    };
+  };
+
+  controls.append(palette); //appends palette table we just created to the controls div in DOM
+
+
+
+  //THIS SECTION TRANSFERS COLOR FROM THE PALETTE TO THE GRID
+
+  $('.palette-cell').click(function() {                  //initiates the event handler with a click on the color pallette
+    clickedColor = $(this).css('background-color');//this holds the color of the clicked tile in variable clickedColor
+    });
+
+  $('.grid-cell').click(function(){                //initiates event handler with a click on grid cell
+    $(this).css("background-color", clickedColor); //transfers color of most recently clickedColor to cell that was just clicked
+  });
+
+  // THIS REMOVES THE LAST COLOR CLICKED
+
+  erase.click(function(){
+    console.log("this")
+    $(this).css('background-color', "");
+    clickedColor = "";
+  });
+
+  //THIS SECTION REMOVES ALL COLORS FROM GRID
+
+  clear.click(function(){
+    $('.grid-cell').css("background-color", "white");
+  });
+
+  };       
+    
+
+
+  $(document).ready(function(){
+    pixelPainter(10,10);
+  });
 
 
 
